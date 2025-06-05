4cd069a19455f7fc62ea997d07da8d3b50cdd9cf
Santi@LCD <santiagohenn@unc.edu.ar>
2025-06-05 16:16:13 -0300

    Plot x axis in seconds

--- plot_ts.py
+++ plot_ts.py
@@ -34,15 +34,15 @@ def main():
             if len(parts) < 3:
                 continue  # skip malformed lines
 
-            sender_sent_at_relative.append(float(parts[1]))
-            receiver_sent_at_relative.append(float(parts[2]))
+            sender_sent_at_relative.append(float(parts[1]) * 1e-3)
+            receiver_sent_at_relative.append(float(parts[2]) * 1e-3)
             sender_to_receiver.append(float(parts[3]))
             receiver_to_sender.append(float(parts[4]))
 
     plt.figure(figsize=(10, 6))
     plt.plot(sender_sent_at_relative, sender_to_receiver, label='Lab -> SL -> Server', alpha=0.5, marker='o', markersize=2)
-    plt.plot(receiver_sent_at_relative, receiver_to_sender, label='Server -> SL -> Lab', alpha=0.5, marker='o', markersize=2)
-    plt.xlabel('Time')
+    # plt.plot(receiver_sent_at_relative, receiver_to_sender, label='Server -> SL -> Lab', alpha=0.5, marker='o', markersize=2)
+    plt.xlabel('Time [s]')
     plt.ylabel('Latency [ms]')
     plt.title('Processed Latency Data')
     plt.legend()
